5096f139a5792ddb18d9a6e97e552bb24364cb36
David Kooistra <dkooi@umich.edu>
2018-01-07 21:58:10 -0500

    Initial Commit

---
+++ Multipeer test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Multipeer test
+//
+//  Created by David Kooistra on 1/7/18.
+//  Copyright © 2018 David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
